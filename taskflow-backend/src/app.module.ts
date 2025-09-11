205c641f9b748c6e9810a5a89abe93881c97bc4a
Vova <vovakopach9@gmail.com>
2025-09-11 22:49:34 +0300

    connect AppController for health-check /

--- taskflow-backend/src/app.module.ts
+++ taskflow-backend/src/app.module.ts
@@ -4,6 +4,7 @@ import { AuthModule } from './auth/auth.module'
 import { TaskModule } from './task/task.module'
 
 import { UserModule } from './user/user.module'
+import { AppController } from './app.controller'
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { UserModule } from './user/user.module'
     AuthModule,
     UserModule,
     TaskModule,
+    AppController
   ],
 })
 export class AppModule {}
